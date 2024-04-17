454ebf4bec9bd065f8d31486a3d0951446d049d1
Alex-Caesar <alexander.caesar@aol.com>
2024-04-16 19:54:48 -0500

    name

--- exercise1/terraform/main.tf
+++ exercise1/terraform/main.tf
@@ -386,7 +386,7 @@ resource "azurerm_application_gateway" "ex1_app_gw" {
 
 resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "ex1_app_gw_nic_asso" {
   network_interface_id    = azurerm_network_interface.ex1_nic_vm.id
-  ip_configuration_name   = "${var.rg_name}_vm_nic_app_gw_asso"
+  ip_configuration_name   = "${var.rg_name}_vm_nic_app_gw_asso_config"
   backend_address_pool_id = tolist(azurerm_application_gateway.ex1_app_gw.backend_address_pool).0.id
 
   depends_on = [azurerm_application_gateway.ex1_app_gw]
