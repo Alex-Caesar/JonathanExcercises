65171ba6793c0d806ac47d7f7394dfe014679b37
alexcaesar <alexjcaesar@yahoo.com>
2024-05-02 11:50:52 -0500

    delegated subnet

--- exercise2/databases.tf
+++ exercise2/databases.tf
@@ -7,20 +7,20 @@ resource "azurerm_subnet" "ex2_subnet_pe" {
   name                 = "${var.rg_name}-subnet-pe"
   resource_group_name  = azurerm_resource_group.ex2.name
   virtual_network_name = azurerm_virtual_network.ex2_vnet.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Storage"]
+  delegation {
+    name = "fs"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+      ]
+    }
 }
-resource "azurerm_subnet_network_security_group_association" "ex2_secg_asso_pe" {
-  subnet_id                 = azurerm_subnet.ex2_subnet_pe.id
-  network_security_group_id = azurerm_network_security_group.ex2_sql_netsecg.id
-}
-resource "azurerm_network_security_group" "ex2_sql_netsecg" {
-  name                = "${var.rg_name}_pe_netsecg"
-  location            = azurerm_resource_group.ex2.location
-  resource_group_name = azurerm_resource_group.ex2.name
 }
 
 # NSG Rules
-
 # TODO
 
 # Postgres
@@ -64,8 +64,8 @@ resource "azurerm_postgresql_flexible_server" "ex2_psql_serv" {
   location            = azurerm_resource_group.ex2.location
 
   version             = var.psql_ver
-  delegated_subnet_id = azurerm_subnet.example.id
-  private_dns_zone_id = azurerm_private_dns_zone.example.id
+  delegated_subnet_id = azurerm_subnet.ex2_subnet_pe.id
+  private_dns_zone_id = azurerm_private_dns_zone.ex2_priv_dns_zone_psql.id
 
   administrator_login    = var.psql_admin
   administrator_password = var.psql_password
