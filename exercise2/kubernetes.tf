2177dc024fcc5841152bf7823bda8c4390a55d73
alexcaesar <alexjcaesar@yahoo.com>
2024-05-02 09:34:46 -0500

    adding k8s

--- exercise2/kubernetes.tf
+++ exercise2/kubernetes.tf
@@ -1,6 +1,25 @@
 # ***************************  AKS related resources ************************************
 
+# __________________________  Kubernetes ________________________________________
 
+resource "azurerm_kubernetes_cluster" "ex2_aks" {
+  name                = "${var.rg_name}-aks-${random_integer.number.result}"
+  resource_group_name = azurerm_resource_group.ex2.name
+  location            = azurerm_resource_group.ex2.location
+  dns_prefix          = "ex2aks"
+
+  default_node_pool {
+    name                        = "default"
+    node_count                  = 1
+    vm_size                     = "Standard_D2_V2"
+    temporary_name_for_rotation = "temp-${var.rg_name}-aks-${random_integer.number.result}"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
 
 # __________________________  Container Registry ________________________________________
 resource "azurerm_container_registry" "ex2_acr" {
